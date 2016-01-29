5a0aeeeaade39b203f5476fe29e6182d41e26a55
alessandrotraversi <alessandro.traversi@hotmail.it>
2016-01-29 17:37:46 -0200

    correzione prog1 e prog4

--- resources/views/public/prog1/index.blade.php
+++ resources/views/public/prog1/index.blade.php
@@ -31,13 +31,13 @@
     <article class="contenuti_descrizione">
       <header>
         <div class="filetto"></div>
+        <h1>Primeira Igreja Presb. de BH<a href="{{route('public.progetto_2')}}" title="vai ao pr&amp;oacute;ximo projeto" class="fa fa-chevron-right"></a>
+        </h1>
+        <h2><i class="fa fa-home"></i>Design de Igrejas<br/>&#35; ProjetosIgrejasEvang&eacute;licas, &#35; Reformas, &#35; DesignDeIgrejas, &#35; DesignDeInterioresBH
+        </h2>
+        <h3>Revitalizaç&atilde;o da 1a. Igreja Presbiteriana de BH: valorizando a historia, regenerando vidas.
+        </h3>
       </header>
-      <h1>Primeira Igreja Presb. de BH<a href="{{route('public.progetto_2')}}" title="vai ao pr&amp;oacute;ximo projeto" class="fa fa-chevron-right"></a>
-      </h1>
-      <h2><i class="fa fa-home"></i>Design de Igrejas<br/>&#35; ProjetosIgrejasEvang&eacute;licas, &#35; Reformas, &#35; DesignDeIgrejas, &#35; DesignDeInterioresBH
-      </h2>
-      <h3>Revitalizaç&atilde;o da 1a. Igreja Presbiteriana de BH: valorizando a historia, regenerando vidas.
-      </h3>
     </article>
     <div class="filetto"></div>
     <p>Inspirado nos atributos da Oliveira, como adaptação e regeneração, o escopo do projeto é externar saúde e vida. O ponto de partida foi a valorização da história e a tradição da instituição. O principal desafio é aprimorar a funcionalidade e revigorar as instalações para melhor atender aos usuários e à dinâmica do crescimento de atividades em proporção ao espaço.<br/><br/><span class="desc"><span class="bold">Conceito:</span> Oliveira<br/><span class="bold">Caracteristicas essenciais:</span> acessibilidade, versatilidade, acolhimento<br/><span class="bold">Demandas</span>: Design de interiores salão de eventos, banheiros públicos, cozinha, ambiente para café e paisagismo.<br/></span>
--- resources/views/public/prog4/_partial.blade.php
+++ resources/views/public/prog4/_partial.blade.php
@@ -12,19 +12,50 @@
     <article class="contenuti_descrizione">
       <header>
         <div class="filetto"></div>
+        <h1>Igreja Presb. de Lajinha/MG<a href="{{route('public.progetto_5')}}" title="vai ao pr&amp;oacute;ximo projeto" class="fa fa-chevron-right"></a><a href="{{route('public.progetto_3')}}" title="volta ao projeto anterior" class="fa fa-chevron-left"></a>
+        </h1>
+        <h2><i class="fa fa-home">Design de Igrejas</i><br/>&#35;ProjetosIgrejasEvang&eacute;licas, &#35;Reformas, &#35;DesignDeIgrejasMG, &#35;Lajinha-MG
+        </h2>
+        <h3>Igreja de Lajinha-MG: Edificando Vidas.
+        </h3>
       </header>
-      <h1>Igreja Presb. de Lajinha/MG<a href="{{route('public.progetto_5')}}" title="vai ao pr&amp;oacute;ximo projeto" class="fa fa-chevron-right"></a><a href="{{route('public.progetto_3')}}" title="volta ao projeto anterior" class="fa fa-chevron-left"></a>
-      </h1>
-      <h2><i class="fa fa-home">Design de Igrejas</i><br/>&#35;ProjetosIgrejasEvang&eacute;licas, &#35;Reformas, &#35;DesignDeIgrejasMG, &#35;Lajinha-MG
-      </h2>
-      <h3>Igreja de Lajinha-MG: Edificando Vidas.
-      </h3>
+      <div class="filetto"></div>
+      <p>O propósito do projeto é fazer uma analogia do processo construtivo com a edificação espiritual da igreja, que é constante. A estrutura e os materiais de construção aparentes são evidenciados para lembrar cotidianamente da obra espiritual de Deus na vida da Igreja.
+      </p>
     </article>
-    <div class="filetto"></div>
-    <p>O propósito do projeto é fazer uma analogia do processo construtivo com a edificação espiritual da igreja, que é constante. A estrutura e os materiais de construção aparentes são evidenciados para lembrar cotidianamente da obra espiritual de Deus na vida da Igreja.<br/><br/><span class="desc"><span class="bold">Conceito:</span> Dinamismo<br/><span class="bold">Caracteristicas essenciais:</span> harmonia, aconchego, espontaneidade, paz.<br/><span class="bold">Demandas:</span> Projetar púlpito, palco, guarda-corpo da galeria (mezanino); Estudo do posicionamento da mesa de som e berçário; Ambientação do berçário e Hall; Rebaixamento de teto sobre o púlpito; Projeto de piso para o interior do templo.</span>
-    </p>
+    <article class="contenuti_concept">
+      <header></header>
+      <p>O grande desafio de projetar o novo templo comemorativo dos 100 anos desta igreja foi aceito por nossa equipe com muita satisfação e responsabilidade.<br /><br />Um templo é em primeiro lugar um santuário ao Deus altíssimo e também representa a igreja (comunidade) que se reúne neste recinto. Projetar o templo de uma igreja tradicional e centenária que tem a preocupação de ser atual e contemporânea nos levou a confrontar paradoxos.<br /><br />O resultado foi um paralelo do templo antigo, com influência no estilo clássico,  que representa a história da instituição e o templo novo: um olhar para o futuro, no estilo contemporâneo e muito despojado, se adornando da simplicidade dos mateirias construtivos e apresentando a circulação e confraternização dos usuários no seu inteiror, como o mais importante elemento de composição.
+      </p>
+    </article>
+    <article class="contenuti_dettagli1">
+      <header></header>
+      <p>A solução em design foi o diálogo entre estilos e épocas, história e visão, respeito e ousadia. Sendo assim, o templo antigo dá lugar a um templo novo, elaborado para atender as demandas atuais e futuras, oferecendo novas tecnologias e parâmetros de conforto. Mas toda a memória de sua história será preservada no prédio antigo, que terá a nova função de salão social e um abrigará um singelo museu.
+      </p>
+    </article>
+    <article class="contenuti_dettagli2">
+      <header></header>
+      <p>Os jardins no estilo tropical possiblitam a contemplação e o relaxamento, contribuindo para o   papel terapêutico da igreja a vida das pessoas.
+      </p>
+    </article>
   </div>
   <ul id="navigazione" class="visible">
+    <li id="descrizione" class="cambia_scheda nonselezionato">
+      <div class="ruota">Conceito
+      </div>
+    </li>
+    <li id="concept" class="cambia_scheda nonselezionato">
+      <div class="ruota">Solu&ccedil;&atilde;o
+      </div>
+    </li>
+    <li id="dettagli1" class="cambia_scheda nonselezionato">
+      <div class="ruota">Detalhes
+      </div>
+    </li>
+    <li id="dettagli2" class="cambia_scheda nonselezionato">
+      <div class="ruota">Detalhes
+      </div>
+    </li>
     <li id="apri_chiudi" class="nonselezionato fa"></li>
   </ul>
 </section>
